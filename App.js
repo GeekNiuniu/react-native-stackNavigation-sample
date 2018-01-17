ee774609c663173b81de1f99120fa2aa81455996
gouthamkd <you@example.com>
2018-01-17 22:46:03 +0530

    react navigation stacknavigator WIP

---
+++ App.js
@@ -0,0 +1,75 @@
+import React from 'react';
+
+import { StackNavigator } from 'react-navigation';
+ import screen1 from './components/stackScreens/screen1';
+import screen2 from './components/stackScreens/screen2';
+import screen3 from './components/stackScreens/screen3';
+
+const App = StackNavigator({
+  screen1: { screen: screen1},
+  screen2: { screen: screen2},
+  screen3: {screen: screen3}
+})
+
+export default App;
+
+
+
+
+// /**
+//  * Sample React Native App
+//  * https://github.com/facebook/react-native
+//  * @flow
+//  */
+
+// import React, { Component } from 'react';
+// import {
+//   Platform,
+//   StyleSheet,
+//   Text,
+//   View
+// } from 'react-native';
+
+// const instructions = Platform.select({
+//   ios: 'Press Cmd+R to reload,\n' +
+//     'Cmd+D or shake for dev menu',
+//   android: 'Double tap R on your keyboard to reload,\n' +
+//     'Shake or press menu button for dev menu',
+// });
+
+// export default class App extends Component<{}> {
+//   render() {
+//     return (
+//       <View style={styles.container}>
+//         <Text style={styles.welcome}>
+//           Welcome to React Native!
+//         </Text>
+//         <Text style={styles.instructions}>
+//           To get started, edit App.js
+//         </Text>
+//         <Text style={styles.instructions}>
+//           {instructions}
+//         </Text>
+//       </View>
+//     );
+//   }
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//     backgroundColor: '#F5FCFF',
+//   },
+//   welcome: {
+//     fontSize: 20,
+//     textAlign: 'center',
+//     margin: 10,
+//   },
+//   instructions: {
+//     textAlign: 'center',
+//     color: '#333333',
+//     marginBottom: 5,
+//   },
+// });
